ace61dd8ce22777523570f2a6010960a0d4ba76c
pafk <laurence2000@gmail.com>
2014-09-06 19:16:00 -0400

    Create stage2.js

---
+++ stage2.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $("#disambigbox").remove();
+    $(".mw-editsection").remove();
+    $(".plainlinks").remove();
+    $(".toc").remove()
+    $to = $("#to").html();
+    $from = $("#from").html();
+    $array = jQuery.parseJSON($("#array").html());
+    $("#to").remove();
+    $("#from").remove();
+    $("#array").remove();
+    $dis = 0;
+    for (i = 0; i < $array.length; i++) {
+        if ($array[i][1] == $from) {
+            $dis = $array[i];
+	    $dis[2] = decodeURI($dis[2]);
+            break;
+        }
+    }
+    $("a").each(function() {
+        $link = decodeURI($(this).attr('href'));
+	console.log($dis[2]);
+        $link = $link.replace("/wiki/", "");
+        if ($link.indexOf($dis[2]) != -1) {
+            $link = $link.replace($dis[2], "")
+	    $link = "stage2.php?t=" + $to + "&f=" +  $from + "&w="+$link;
+        }
+	else
+	{
+	    $link = "stage3.php?t=" + $to + "&f=" +  $from + "&w="+$link;
+	}
+	$(this).attr('href', $link);
+    });
+});
